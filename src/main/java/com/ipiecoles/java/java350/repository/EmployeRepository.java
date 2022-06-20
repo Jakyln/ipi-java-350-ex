432df2c55d6609d50c608b97ebadd8dd1a067744
Julien <kalaycijulien@gmail.com>
2022-06-20 12:34:11 +0200

    ajout de test unitaires méthode embauche

--- src/main/java/com/ipiecoles/java/java350/repository/EmployeRepository.java
+++ src/main/java/com/ipiecoles/java/java350/repository/EmployeRepository.java
@@ -9,6 +9,7 @@ import java.util.List;
 
 @Repository
 public interface EmployeRepository extends JpaRepository<Employe, Long> {
+    //On récupère le dernier matricule , ordonnée par les chiffres du matricul
     @Query("select max(substring(matricule,2)) from Employe")
     String findLastMatricule();
 
